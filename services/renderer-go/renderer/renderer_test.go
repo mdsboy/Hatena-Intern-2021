48f772d58e4d55c4bd93ff31b686a9e1d43f8b1f
Yu Daiki <daiki.y.aa@m.titech.ac.jp>
2021-08-18 16:27:21 +0900

    テストを必須課題の内容に変更

--- services/renderer-go/renderer/renderer_test.go
+++ services/renderer-go/renderer/renderer_test.go
@@ -7,9 +7,36 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_Render(t *testing.T) {
-	src := `foo https://google.com/ bar`
+func Test_Render_Header(t *testing.T) {
+	src := `# h1
+## h2
+### h3
+#### h4`
 	html, err := Render(context.Background(), src)
 	assert.NoError(t, err)
-	assert.Equal(t, `foo <a href="https://google.com/">https://google.com/</a> bar`, html)
+	assert.Equal(t, `<h1>h1</h1>
+<h2>h2</h2>
+<h3>h3</h3>
+<h4>h4</h4>
+`, html)
+}
+
+func Test_Render_List(t *testing.T) {
+	src := `- list1
+- list2`
+	html, err := Render(context.Background(), src)
+	assert.NoError(t, err)
+	assert.Equal(t, `<ul>
+<li>list1</li>
+<li>list2</li>
+</ul>
+`, html)
+}
+
+func Test_Render_Link(t *testing.T) {
+	src := `[google.com](https:google.com)`
+	html, err := Render(context.Background(), src)
+	assert.NoError(t, err)
+	assert.Equal(t, `<p><a href="https:google.com">google.com</a></p>
+`, html)
 }
